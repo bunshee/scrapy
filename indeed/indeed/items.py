c2a9f2e095639ba51abf5d32d8d100b601bee6f9
Gorchene Badr <52808107+bunshee@users.noreply.github.com>
2022-04-28 17:33:59 +0100

    Update items.py

--- indeed/indeed/items.py
+++ indeed/indeed/items.py
@@ -1,13 +1,7 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
-
 class IndeedItem(scrapy.Item):
-    # define the fields for your item here like:
+
     jobTitle = scrapy.Field()
     companyName = scrapy.Field()
     jobPlace = scrapy.Field()
